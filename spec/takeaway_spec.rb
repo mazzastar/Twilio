469cdf9931543eb03cff24a7fb3d5b0f322dbd14
Alexander Wong Makers <aw12409@my.bristol.ac.uk>
2014-04-25 20:29:53 +0100

    first commit

---
+++ spec/takeaway_spec.rb
@@ -0,0 +1,5 @@
+require "takeaway"
+
+def Takeaway
+	
+end
---
+++ test.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'twilio-ruby'
+ 
+# # Get your Account Sid and Auth Token from twilio.com/user/account
+# account_sid = 'AC36b0a7b7aef8d0cab1605860a40edc8c'
+# auth_token = 'cf3269478f796d5af3d16a47d10a3fed'
+# @client = Twilio::REST::Client.new account_sid, auth_token
+ 
+# message = @client.account.sms.messages.create(:body => "Jenny please?! I love you <3",
+#     :to => "+447814430586",     # Replace with your phone number
+#     :from => "+441562612030")   # Replace with your Twilio number
+# puts message.sid
+
+class TestTwilio
+	def initialize 
+		account_sid = 'AC6b543fe3489268bcd6a73496cb1a5a9c'
+		auth_token = '45e4638bbf3cb2bd05dc9534920d53bc'
+		@client = Twilio::REST::Client.new account_sid, auth_token
+	end
+
+	def send_text(info)
+		@client.account.messages.create(
+			:from => "+441562612030",
+			:to => "+447814430586",
+			:body => "Thank you! Your order was placed and will be delivered before"
+			)
+	end
+end
